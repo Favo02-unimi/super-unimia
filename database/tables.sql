b283adc8a14be857d5f0d832816c67129743f7b8
Luca Favini <luca.favini.02@gmail.com>
2023-08-06 23:12:43 +0200

    db(tables): edit `utenti` table creation

---
+++ database/tables.sql
@@ -0,0 +1,24 @@
+SET search_path TO unimia;
+
+DROP TABLE utenti CASCADE;
+CREATE TABLE utenti (
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  password TEXT NOT NULL,
+  nome TEXT NOT NULL CHECK (nome ~* '^.+$'),
+  cognome TEXT NOT NULL CHECK (cognome ~* '^.+$'),
+  tipo TIPO_UTENTE NOT NULL,
+  email TEXT NOT NULL UNIQUE CHECK (email ~* '^[A-Za-z0-9._%-]+@(studente|docente|segretario).superuni.it$')
+);
+
+CREATE TABLE studenti (
+  id uuid REFERENCES utenti(id),
+  matricola CHAR(6) NOT NULL UNIQUE CHECK (matricola ~* '^\d{6}$')
+);
+
+CREATE TABLE docenti (
+  id uuid REFERENCES utenti(id)
+);
+
+CREATE TABLE segretari (
+  id uuid REFERENCES utenti(id)
+);
