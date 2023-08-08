b2690f1b1dd718c711f44811a53c7ef25aea13e6
Luca Favini <luca.favini.02@gmail.com>
2023-08-08 23:22:12 +0200

    web(pages): add studente `navbar`

---
+++ webapp/studente/navbar.php
@@ -0,0 +1,27 @@
+<!-- component to be included by other files -->
+
+<nav class="c-navbar navbar is-spaced">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="home.php">
+      <h1 class="title">SuperUnimia</h1>
+    </a>
+  </div>
+
+  <div class="navbar-menu">
+    <div class="navbar-start ml-4">
+      <a class="navbar-item" href="">Home</a>
+      <a class="navbar-item" href="">Home</a>
+      <a class="navbar-item" href="">Home</a>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-link" href="../scripts/logout.php">
+            <strong>Logout</strong>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
