054bc27be674b97e5b93e495d0b74aaef49905b8
Luca Favini <luca.favini.02@gmail.com>
2023-08-08 19:39:17 +0200

    db(procedures): add `new_docente`, `new_segretario`

--- database/procedures.sql
+++ database/procedures.sql
@@ -1,5 +1,6 @@
 SET search_path TO unimia;
 
+-- crea un nuovo studente dato nome, cognome e password
 CREATE OR REPLACE PROCEDURE new_studente (
   _nome TEXT,
   _cognome TEXT,
@@ -19,6 +20,54 @@ CREATE OR REPLACE PROCEDURE new_studente (
       -- TODO: insert into studente
       -- INSERT INTO studenti(id, matricola)
       -- VALUES (_id, '000002');
+      
+    END;
+  $$;
+
+-- crea un nuovo docente dato nome, cognome e password
+CREATE OR REPLACE PROCEDURE new_docente (
+  _nome TEXT,
+  _cognome TEXT,
+  _password TEXT
+)
+  LANGUAGE plpgsql
+  AS $$
+    DECLARE _id uuid;
+    BEGIN
+
+      SET search_path TO unimia;
+
+      INSERT INTO utenti(password, nome, cognome, tipo)
+      VALUES (_password, _nome, _cognome, 'docente')
+      RETURNING id INTO _id;
+
+      -- TODO: insert into docente
+      INSERT INTO docenti(id)
+      VALUES (_id);
+      
+    END;
+  $$;
+
+-- crea un nuovo segretario dato nome, cognome e password
+CREATE OR REPLACE PROCEDURE new_segretario (
+  _nome TEXT,
+  _cognome TEXT,
+  _password TEXT
+)
+  LANGUAGE plpgsql
+  AS $$
+    DECLARE _id uuid;
+    BEGIN
+
+      SET search_path TO unimia;
+
+      INSERT INTO utenti(password, nome, cognome, tipo)
+      VALUES (_password, _nome, _cognome, 'segretario')
+      RETURNING id INTO _id;
+
+      -- TODO: insert into docente
+      INSERT INTO segretari(id)
+      VALUES (_id);
       
     END;
   $$;
