057477e758b866a6b66b9b02035df51cb52293f5
Luca Favini <luca.favini.02@gmail.com>
2023-08-09 20:20:45 +0200

    web(pages): add `docente`, `segretario` home page

---
+++ webapp/segretario/home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+  <link rel="stylesheet" href="../styles/index.css">
+  <script src="https://kit.fontawesome.com/eb793f993c.js" crossorigin="anonymous"></script>
+  <title>Home segretario - SuperUnimia</title>
+</head>
+<body class="has-background-dark has-text-light">
+
+  <?php
+
+  require_once("../scripts/utils.php");
+
+  $CUR_PAGE = "segretario";
+  require("../scripts/redirector.php");
+
+  require("navbar.php");
+
+  ?>
+
+  <h1>Segretario</h1>
+
+
+</body>
+</html>
