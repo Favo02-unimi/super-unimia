ed21494032aeeb2e169432df7b38d6ad8930318b
Luca Favini <luca.favini.02@gmail.com>
2023-08-10 11:55:28 +0200

    db(functions): add `get_studenti`, `get_docenti`, `get_segretari`

--- database/functions.sql
+++ database/functions.sql
@@ -24,3 +24,67 @@ CREATE OR REPLACE FUNCTION login (
 
     END;
   $$;
+
+-- restituisce tutti gli studenti disponibili
+CREATE OR REPLACE FUNCTION get_studenti ()
+  RETURNS TABLE (
+    __nome TEXT,
+    __cognome TEXT,
+    __email TEXT,
+    __matricola CHAR(6)
+  )
+  LANGUAGE plpgsql
+  AS $$
+    BEGIN
+
+      SET search_path TO unimia;
+
+      RETURN QUERY
+        SELECT u.nome, u.cognome, u.email, s.matricola
+        FROM studenti AS s
+        INNER JOIN utenti AS u ON u.id = s.id;
+
+    END;
+  $$;
+
+-- restituisce tutti i docenti disponibili
+CREATE OR REPLACE FUNCTION get_docenti ()
+  RETURNS TABLE (
+    __nome TEXT,
+    __cognome TEXT,
+    __email TEXT
+  )
+  LANGUAGE plpgsql
+  AS $$
+    BEGIN
+
+      SET search_path TO unimia;
+
+      RETURN QUERY
+        SELECT u.nome, u.cognome, u.email
+        FROM docenti AS d
+        INNER JOIN utenti AS u ON u.id = d.id;
+
+    END;
+  $$;
+
+-- restituisce tutti i segretari disponibili
+CREATE OR REPLACE FUNCTION get_segretari ()
+  RETURNS TABLE (
+    __nome TEXT,
+    __cognome TEXT,
+    __email TEXT
+  )
+  LANGUAGE plpgsql
+  AS $$
+    BEGIN
+
+      SET search_path TO unimia;
+
+      RETURN QUERY
+        SELECT u.nome, u.cognome, u.email
+        FROM segretari AS s
+        INNER JOIN utenti AS u ON u.id = s.id;
+
+    END;
+  $$;
