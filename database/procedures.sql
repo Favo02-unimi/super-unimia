c0d8820057769655548ac9bb41662f858b4a0dcc
Luca Favini <luca.favini.02@gmail.com>
2023-08-17 15:15:42 +0200

    db(procedures): add `delete_studente` procedure

--- database/procedures.sql
+++ database/procedures.sql
@@ -23,6 +23,54 @@ CREATE OR REPLACE PROCEDURE new_studente (
     END;
   $$;
 
+-- modifica uno studente dato il suo id
+-- non vengono aggiornati i campi lasciati a NULL (coalesce)
+CREATE OR REPLACE PROCEDURE edit_studente (
+  _id uuid,
+  _nome TEXT,
+  _cognome TEXT,
+  _email TEXT,
+  _matricola CHAR(6)
+)
+  LANGUAGE plpgsql
+  AS $$
+    BEGIN
+
+      SET search_path TO unimia;
+
+      UPDATE utenti SET
+        nome = COALESCE(NULLIF(_nome, ''), nome),
+        cognome = COALESCE(NULLIF(_cognome, ''), cognome),
+        email = COALESCE(NULLIF(_email, ''), email)
+      WHERE id = _id;
+
+      UPDATE studenti SET
+        matricola = COALESCE(NULLIF(_matricola, ''), matricola)
+      WHERE id = _id;
+
+    END;
+  $$;
+
+-- elimina uno studente dato il suo id
+-- l'utente non viene cancellato in caso siano presenti foreing key
+CREATE OR REPLACE PROCEDURE delete_studente (
+  _id uuid
+)
+  LANGUAGE plpgsql
+  AS $$
+    BEGIN
+
+      SET search_path TO unimia;
+
+      DELETE FROM studenti
+      WHERE id = _id;
+
+      DELETE FROM utenti
+      WHERE id = _id;
+
+    END;
+  $$;
+
 -- crea un nuovo docente dato nome, cognome e password
 CREATE OR REPLACE PROCEDURE new_docente (
   _nome TEXT,
@@ -68,33 +116,5 @@ CREATE OR REPLACE PROCEDURE new_segretario (
       INSERT INTO segretari(id)
       VALUES (_id);
       
-    END;
-  $$;
-
--- modifica uno studente dato il suo id
--- non vengono aggiornati i campi lasciati a NULL (coalesce)
-CREATE OR REPLACE PROCEDURE edit_studente (
-  _id uuid,
-  _nome TEXT,
-  _cognome TEXT,
-  _email TEXT,
-  _matricola CHAR(6)
-)
-  LANGUAGE plpgsql
-  AS $$
-    BEGIN
-
-      SET search_path TO unimia;
-
-      UPDATE utenti SET
-        nome = COALESCE(NULLIF(_nome, ''), nome),
-        cognome = COALESCE(NULLIF(_cognome, ''), cognome),
-        email = COALESCE(NULLIF(_email, ''), email)
-      WHERE id = _id;
-
-      UPDATE studenti SET
-        matricola = COALESCE(NULLIF(_matricola, ''), matricola)
-      WHERE id = _id;
-
     END;
   $$;
