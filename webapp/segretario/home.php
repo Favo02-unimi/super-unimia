79fd806bf11cd3d98d02f5faf4f3346090d464ba
Luca Favini <luca.favini.02@gmail.com>
2023-08-17 16:13:53 +0200

    web(pages): update `segretario` / `home` page
    add links to new features

--- webapp/segretario/home.php
+++ webapp/segretario/home.php
@@ -17,12 +17,66 @@
   $CUR_PAGE = "segretario";
   require("../scripts/redirector.php");
 
-  require("navbar.php");
+  require("../navbar.php");
 
   ?>
 
-  <h1>Segretario</h1>
+  <div class="container is-max-desktop box">
 
+    <?php if (isset($_SESSION["feedback"])): ?>
+      <div class="notification is-success is-light mt-6">
+        <strong><?php echo $_SESSION["feedback"]; unset($_SESSION["feedback"]) ?></strong>
+      </div>
+    <?php endif; ?>
+    
+    <div class="block">
+      <p class="title is-2 is-link">Buongiorno, <?php echo $_SESSION["username"]; ?>.</p>
+      <p class="subtitle is-4">Servizi accessibili come <?php echo $_SESSION["usertype"]; ?>:</p>
+    </div>
 
+    <a href="nuovo_studente.php" class="block button is-link is-outlined is-fullwidth">
+      <span class="icon is-small"><i class="fa-solid fa-user-graduate fa-xl"></i></span>
+      <span class="icon is-small"><i class="fa-regular fa-square-plus"></i></span>
+      <strong>Creazione studenti</strong>
+    </a>
+
+    <a href="gestione_studenti.php" class="block button is-link is-outlined is-fullwidth">
+      <span class="icon is-small"><i class="fa-solid fa-user-graduate fa-xl"></i></span>
+      <span class="icon is-small"><i class="fa-solid fa-list"></i></span>
+      <span class="icon is-small"><i class="fa-solid fa-pen-to-square"></i></span>
+      <span class="icon is-small"><i class="fa-solid fa-trash"></i></span>
+      <strong>Gestione studenti (Lista, Modifica, Cancellazione)</strong>
+    </a>
+
+    <a href="nuovo_docente.php" class="block button is-link is-outlined is-fullwidth">
+      <span class="icon is-small"><i class="fa-solid fa-user-tie fa-xl"></i></span>
+      <span class="icon is-small"><i class="fa-regular fa-square-plus"></i></span>
+      <strong>Creazione docenti</strong>
+    </a>
+
+    <a href="gestione_docenti.php" class="block button is-link is-outlined is-fullwidth">
+      <span class="icon is-small"><i class="fa-solid fa-user-tie fa-xl"></i></span>
+      <span class="icon is-small"><i class="fa-solid fa-list"></i></span>
+      <span class="icon is-small"><i class="fa-solid fa-pen-to-square"></i></span>
+      <span class="icon is-small"><i class="fa-solid fa-trash"></i></span>
+      <strong>Gestione docenti (Lista, Modifica, Cancellazione)</strong>
+    </a>
+
+    <a href="nuovo_segretario.php" class="block button is-link is-outlined is-fullwidth">
+      <span class="icon is-small"><i class="fa-solid fa-user-gear fa-xl"></i></span>
+      <span class="icon is-small"><i class="fa-regular fa-square-plus"></i></span>
+      <strong>Creazione segretari</strong>
+    </a>
+
+    <a href="gestione_segretari.php" class="block button is-link is-outlined is-fullwidth">
+      <span class="icon is-small"><i class="fa-solid fa-user-gear fa-xl"></i></span>
+      <span class="icon is-small"><i class="fa-solid fa-list"></i></span>
+      <span class="icon is-small"><i class="fa-solid fa-pen-to-square"></i></span>
+      <span class="icon is-small"><i class="fa-solid fa-trash"></i></span>
+      <strong>Gestione segretari (Lista, Modifica, Cancellazione)</strong>
+    </a>
+    
+  </div>
+    
 </body>
 </html>
