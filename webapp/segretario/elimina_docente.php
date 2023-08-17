ef9ddd9b14ef2b50a15e125d85b2989b253fa188
Luca Favini <luca.favini.02@gmail.com>
2023-08-17 15:56:25 +0200

    web(pages): add `segretario` / `elimina_docente` page

---
+++ webapp/segretario/elimina_docente.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+  <link rel="stylesheet" href="../styles/index.css">
+  <script src="https://kit.fontawesome.com/eb793f993c.js" crossorigin="anonymous"></script>
+  <title>Elimina docente - SuperUnimia</title>
+</head>
+<body class="has-background-dark has-text-light">
+
+  <?php
+
+  require_once("../scripts/utils.php");
+
+  $CUR_PAGE = "segretario";
+  require("../scripts/redirector.php");
+
+  require("../navbar.php");
+
+  ?>
+
+  <div class="container is-max-desktop">
+
+    <?php
+
+      if (isset($_POST["submit"])) {
+        $qry = "CALL unimia.delete_docente($1);";
+        $res = pg_prepare($con, "", $qry);
+        $res = pg_execute($con, "", array($_POST["id"]));
+
+        if (!$res): ?>
+          <div class="notification is-danger is-light mt-6">
+            <strong>Errore durante la cancellazione:</strong>
+            <?php echo ParseError(pg_last_error()); ?>.
+          </div>
+        <?php else: 
+          $_SESSION["feedback"] = "Docente eliminato con successo.";
+          Redirect("home.php");
+        endif;
+      }
+
+    ?>
+
+      <form class="box p-6" action="" method="post">
+
+      <span class="icon-text">
+        <span class="icon is-large">
+          <i class="fa-solid fa-user-tie fa-2xl"></i>
+        </span>
+        <h1 class="title mt-2">Elimina docente</h1>
+      </span>
+
+      <input class="input" type="hidden" name="id" value="<?php echo $_POST["id"] ?>" required>
+
+      <div class="field mt-5">
+        <p class="control">
+          <input type="submit" name="submit" value="Conferma cancellazione" class="button is-danger is-fullwidth is-medium">
+        </p>
+      </div>
+
+    </form>
+
+  </div>
+    
+</body>
+</html>
