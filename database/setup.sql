16c2d6b5598cb11d353facefd219a761b0a8cc8f
Luca Favini <luca.favini.02@gmail.com>
2023-08-18 18:39:07 +0200

    db(setup): add `pgcrypto` extension

--- database/setup.sql
+++ database/setup.sql
@@ -3,5 +3,8 @@ CREATE SCHEMA unimia;
 
 SET search_path TO unimia;
 
+-- estensione per l'hashing delle password
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
 -- tipi di utenti possibili nel database
 CREATE TYPE TIPO_UTENTE AS ENUM ('studente', 'docente', 'segretario');
