d4103e7532dbe12b3728eb2f8dd031c45b5926ad
Luca Favini <luca.favini.02@gmail.com>
2023-08-25 15:25:13 +0200

    db(tables): fix order of table creation

--- database/tables.sql
+++ database/tables.sql
@@ -9,6 +9,13 @@ CREATE TABLE utenti (
   email TEXT NOT NULL UNIQUE CHECK (email ~* '^[A-Za-z0-9._%-]+@(studente|docente|segretario).superuni.it$')
 );
 
+CREATE TABLE corsi_di_laurea (
+  codice VARCHAR(6) PRIMARY KEY,
+  tipo TIPO_LAUREA NOT NULL,
+  nome TEXT NOT NULL,
+  descrizione TEXT NOT NULL
+);
+
 CREATE TABLE studenti (
   id uuid NOT NULL REFERENCES utenti(id),
   matricola CHAR(6) NOT NULL UNIQUE CHECK (matricola ~* '^\d{6}$'),
@@ -21,11 +28,4 @@ CREATE TABLE docenti (
 
 CREATE TABLE segretari (
   id uuid NOT NULL REFERENCES utenti(id)
-);
-
-CREATE TABLE corsi_di_laurea (
-  codice VARCHAR(6) PRIMARY KEY,
-  tipo TIPO_LAUREA NOT NULL,
-  nome TEXT NOT NULL,
-  descrizione TEXT NOT NULL
 );
