fceddde64e764c76851d7519a9370575f9dd9319
Luca Favini <luca.favini.02@gmail.com>
2023-08-25 22:47:33 +0200

    web(common): update `footer`

--- webapp/footer.php
+++ webapp/footer.php
@@ -2,12 +2,12 @@
 
 <footer class="footer">
   <div class="content has-text-centered has-text-dark	">
-    <p>Built with <abbr class="pointer" title="FUCK PHP, ALL MY HOMIES HATE PHP"><i class="fa-solid fa-brain"></i> but not <i class="fa-solid fa-heart"></i></abbr> by <a href="https://github.com/Favo02"><u>Luca Favini</u></a>.</p>
+    <p>Built with <abbr class="pointer" title="FUCK PHP, ALL MY HOMIES HATE PHP"><i title="brain" class="fa-solid fa-brain"></i> but not <i title="heart" class="fa-solid fa-heart"></i></abbr> by <a target="_blank" href="https://github.com/Favo02"><u>Luca Favini</u></a>.</p>
     
     <p>
-      <u><a href="https://github.com/Favo02/super-unimia">Source code</a></u> - 
-      <u><a href="https://creativecommons.org/licenses/by/4.0/">CC-BY 4.0 Licence</a></u> - 
-      <u><a href="https://github.com/Favo02/super-unimia/issues">Report a bug</a></u>
+      <u><a target="_blank" href="https://github.com/Favo02/super-unimia">Source code</a></u> - 
+      <u><a target="_blank" href="https://creativecommons.org/licenses/by/4.0/">CC-BY 4.0 Licence</a></u> - 
+      <u><a target="_blank" href="https://github.com/Favo02/super-unimia/issues">Report a bug</a></u>
     </p>
   </div>
 </footer>
