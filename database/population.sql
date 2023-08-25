faf22185262e99aa35ecffafb0c04a5479c831a4
Luca Favini <luca.favini.02@gmail.com>
2023-08-25 15:46:37 +0200

    db(population): update initial `population`

--- database/population.sql
+++ database/population.sql
@@ -1,1 +1,10 @@
-CALL unimia.new_segretario('seg', 'retario', '8Caratteri!');
+SET search_path TO unimia;
+
+CALL new_segretario('seg', 'retario', '8Caratteri!');
+
+CALL new_corso_di_laurea('L-31', 'Triennale', 'Informatica', 'Pura, non ingegneri');
+CALL new_corso_di_laurea('CU-01', 'Magistrale a ciclo unico', 'Medicina', 'Curatemi da PHP');
+
+CALL new_studente('stu', 'dente', '8Caratteri!', 'L-31');
+CALL new_docente('doc', 'ente', '8Caratteri!');
+
