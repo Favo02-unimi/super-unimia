b4e65f52c9709bcb179b9befcbe178079a927099
Luca Favini <luca.favini.02@gmail.com>
2023-08-25 22:34:33 +0200

    web(common): add `footer`

---
+++ webapp/footer.php
@@ -0,0 +1,13 @@
+<!-- component to be included by other files -->
+
+<footer class="footer">
+  <div class="content has-text-centered has-text-dark	">
+    <p>Built with <abbr class="pointer" title="FUCK PHP, ALL MY HOMIES HATE PHP"><i class="fa-solid fa-brain"></i> but not <i class="fa-solid fa-heart"></i></abbr> by <a href="https://github.com/Favo02"><u>Luca Favini</u></a>.</p>
+    
+    <p>
+      <u><a href="https://github.com/Favo02/super-unimia">Source code</a></u> - 
+      <u><a href="https://creativecommons.org/licenses/by/4.0/">CC-BY 4.0 Licence</a></u> - 
+      <u><a href="https://github.com/Favo02/super-unimia/issues">Report a bug</a></u>
+    </p>
+  </div>
+</footer>
