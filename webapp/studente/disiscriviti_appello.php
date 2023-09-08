1a278354b43d41e07ed1d7824c4fa06d606833da
Luca Favini <luca.favini.02@gmail.com>
2023-09-08 16:34:48 +0200

    web(studente): convert `form` pages to `form` component

--- webapp/studente/disiscriviti_appello.php
+++ webapp/studente/disiscriviti_appello.php
@@ -1,120 +1,93 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-  <link rel="stylesheet" href="../styles/index.css">
-  <script src="https://kit.fontawesome.com/eb793f993c.js" crossorigin="anonymous"></script>
-  <title>Disiscriviti appello - SuperUnimia</title>
-</head>
-<body class="has-background-dark has-text-light">
+<?php
 
-  <?php
+require_once("../scripts/utils.php");
 
-  require_once("../scripts/utils.php");
+if (isset($_POST["submit"])) {
+  $qry = "CALL unimia.disiscriviti_appello($1, $2);";
+  $res = pg_prepare($con, "", $qry);
+  $res = pg_execute($con, "", array($_SESSION["userid"], $_POST["codice"]));
 
-  $CUR_PAGE = "studente";
-  require("../scripts/redirector.php");
+  if (!$res) {
+    $error = ParseError(pg_last_error());
+  }
+  else {
+    unset($error);
+    $_SESSION["feedback"] = "Discrizione avvenuta con successo.";
+    Redirect("home.php");
+  }
+}
 
-  require("../navbar.php");
+$CUR_PAGE = "studente";
+$fa_icon = "fa-calendar-day";
+$title = "Disiscriviti appello";
+$subtitle = "Disiscriviti dall'appello selezionato.";
 
-  ?>
+$class = "is-danger";
+$help = "Solo se non è un appello passato";
 
-  <div class="container is-max-desktop">
+$inputs = array(
+  array(
+    "type"=>"hidden",
+    "name"=>"codice",
+    "value"=>$_POST["codice"]
+  ),
+  array(
+    "type"=>"text",
+    "label"=>"Insegnamento",
+    "name"=>"nome_insegnamento",
+    "value"=>$_POST["insegnamento"]." - ".$_POST["nome_insegnamento"],
+    "placeholder"=>"",
+    "required"=>"required",
+    "readonly"=>"readonly",
+    "icon"=>"fa-book",
+    "help"=>""
+  ),
+  array(
+    "type"=>"text",
+    "label"=>"Data",
+    "name"=>"data",
+    "value"=>$_POST["data"],
+    "placeholder"=>"",
+    "required"=>"required",
+    "readonly"=>"readonly",
+    "icon"=>"fa-calendar-days",
+    "help"=>""
+  ),
+  array(
+    "type"=>"text",
+    "label"=>"Ora",
+    "name"=>"ora",
+    "value"=>$_POST["ora"],
+    "placeholder"=>"",
+    "required"=>"required",
+    "readonly"=>"readonly",
+    "icon"=>"fa-clock",
+    "help"=>""
+  ),
+  array(
+    "type"=>"text",
+    "label"=>"Luogo",
+    "name"=>"luogo",
+    "value"=>$_POST["luogo"],
+    "placeholder"=>"",
+    "required"=>"required",
+    "readonly"=>"readonly",
+    "icon"=>"fa-location-dot",
+    "help"=>""
+  ),
+  array(
+    "type"=>"text",
+    "label"=>"Ultimo voto",
+    "name"=>"ultimo_voto",
+    "value"=>$_POST["ultimo_voto"] == "" ? "Mai sostenuto" : $_POST["ultimo_voto"],
+    "placeholder"=>"",
+    "required"=>"required",
+    "readonly"=>"readonly",
+    "icon"=>"fa-marker",
+    "help"=>""
+  )
+);
 
-    <?php
+require("../components/form.php");
 
-      if (isset($_POST["submit"])) {
-        $qry = "CALL unimia.disiscriviti_appello($1, $2);";
-        $res = pg_prepare($con, "", $qry);
-        $res = pg_execute($con, "", array($_SESSION["userid"], $_POST["codice"]));
-
-        if (!$res): ?>
-          <div class="notification is-danger is-light mt-6">
-            <strong>Errore durante la creazione:</strong>
-            <?php echo ParseError(pg_last_error()); ?>.
-          </div>
-        <?php else: 
-          $_SESSION["feedback"] = "Iscrizione avvenuta con successo.";
-          Redirect("home.php");
-        endif;
-      }
-    ?>
-    
-    <form class="box p-6" action="" method="post">
-
-      <span class="icon-text">
-        <span class="icon is-large">
-          <i class="fa-solid fa-calendar-day fa-2xl"></i>
-        </span>
-        <h1 class="title mt-2">Disiscriviti appello</h1>
-      </span>
-
-      <input type="hidden" name="codice" value="<?php echo $_POST["codice"] ?>">
-
-      <label class="label mt-5">Insegnamento</label>
-      <div class="field">
-        <p class="control has-icons-left">
-          <input class="input" type="text" value="<?php echo $_POST["insegnamento"] ?> - <?php echo $_POST["nome_insegnamento"] ?>" required readonly>
-          <span class="icon is-small is-left">
-            <i class="fa-solid fa-book"></i>
-          </span>
-        </p>
-      </div>
-
-      <label class="label mt-5">Data</label>
-      <div class="field">
-        <p class="control has-icons-left">
-          <input class="input" type="text" value="<?php echo $_POST["data"] ?>" required readonly>
-          <span class="icon is-small is-left">
-            <i class="fa-solid fa-calendar-days"></i>
-          </span>
-        </p>
-      </div>
-
-      <label class="label mt-5">Ora</label>
-      <div class="field">
-        <p class="control has-icons-left">
-          <input class="input" type="text" value="<?php echo $_POST["ora"] ?>" required readonly>
-          <span class="icon is-small is-left">
-            <i class="fa-solid fa-clock"></i>
-          </span>
-        </p>
-      </div>
-
-      <label class="label mt-5">Luogo</label>
-      <div class="field">
-        <p class="control has-icons-left">
-          <input class="input" type="text" value="<?php echo $_POST["luogo"] ?>" required readonly>
-          <span class="icon is-small is-left">
-            <i class="fa-solid fa-location-dot"></i>
-          </span>
-        </p>
-      </div>
-
-      <label class="label mt-5">Ultimo voto</label>
-      <div class="field">
-        <p class="control has-icons-left">
-          <input class="input" type="text" value="<?php echo $_POST["ultimo_voto"] == "" ? "Mai sostenuto" : $_POST["ultimo_voto"] ?>" required readonly>
-          <span class="icon is-small is-left">
-            <i class="fa-solid fa-marker"></i>
-          </span>
-        </p>
-        <p class="help">Questo voto (se presente) verrà sovrascritto dal risultato di questo appello.</p>
-      </div>
-
-      <div class="field mt-5">
-        <p class="control">
-          <input type="submit" name="submit" value="Disiscriviti appello" class="button is-danger is-fullwidth is-medium">
-        </p>
-      </div>
-
-    </form>
-  
-  </div>
-    
-  <?php require("../footer.php"); ?>
-
-</body>
-</html>
+?>
--- webapp/studente/modifica_password.php
+++ webapp/studente/modifica_password.php
@@ -1,88 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-  <link rel="stylesheet" href="../styles/index.css">
-  <script src="https://kit.fontawesome.com/eb793f993c.js" crossorigin="anonymous"></script>
-  <title>Modifica password - SuperUnimia</title>
-</head>
-<body class="has-background-dark has-text-light">
+<?php
 
-  <?php
+require_once("../scripts/utils.php");
 
-  require_once("../scripts/utils.php");
+if (isset($_POST["submit"])) {
+  $qry = "CALL unimia.edit_password($1, $2, $3);";
+  $res = pg_prepare($con, "", $qry);
+  $res = pg_execute($con, "", array($_SESSION["userid"], $_POST["old_password"], $_POST["password"]));
 
-  $CUR_PAGE = "studente";
-  require("../scripts/redirector.php");
+  if (!$res) {
+    $error = ParseError(pg_last_error());
+  }
+  else {
+    unset($error);
+    $_SESSION["feedback"] = "Password aggiornata con successo.";
+    Redirect("home.php");
+  }
+}
 
-  require("../navbar.php");
+$CUR_PAGE = "studente";
+$fa_icon = "fa-lock";
+$title = "Modifica password";
+$subtitle = "";
 
-  ?>
+$class = "is-link";
+$help = "";
 
-  <div class="container is-max-desktop">
+$inputs = array(
+  array(
+    "type"=>"password",
+    "label"=>"Vecchia password",
+    "name"=>"old_password",
+    "value"=>"",
+    "placeholder"=>"Vecchia password",
+    "required"=>"required",
+    "readonly"=>"",
+    "icon"=>"fa-lock",
+    "help"=>""
+  ),
+  array(
+    "type"=>"password",
+    "label"=>"Nuova password",
+    "name"=>"password",
+    "value"=>"",
+    "placeholder"=>"Nuova password",
+    "required"=>"required",
+    "readonly"=>"",
+    "icon"=>"fa-lock",
+    "help"=>""
+  )
+);
 
-    <?php
+require("../components/form.php");
 
-      if (isset($_POST["submit"])) {
-        $qry = "CALL unimia.edit_password($1, $2, $3);";
-        $res = pg_prepare($con, "", $qry);
-        $res = pg_execute($con, "", array($_SESSION["userid"], $_POST["old_password"], $_POST["password"]));
-
-        if (!$res): ?>
-          <div class="notification is-danger is-light mt-6">
-            <strong>Errore durante la modifica:</strong>
-            <?php echo ParseError(pg_last_error()); ?>.
-          </div>
-        <?php else: 
-          $_SESSION["feedback"] = "Password aggiornata con successo.";
-          Redirect("home.php");
-        endif;
-      }
-
-    ?>
-
-    <form class="box p-6" action="" method="post">
-
-      <span class="icon-text">
-        <span class="icon is-large">
-          <i class="fa-solid fa-lock fa-2xl"></i>
-        </span>
-        <h1 class="title mt-2">Modifica password</h1>
-      </span>
-
-      <label class="label mt-5">Vecchia password</label>
-      <div class="field">
-        <p class="control has-icons-left">
-          <input class="input" type="password" name="old_password" placeholder="Password" required>
-          <span class="icon is-small is-left">
-            <i class="fa-solid fa-lock"></i>
-          </span>
-        </p>
-      </div>
-
-      <label class="label mt-5">Nuova password</label>
-      <div class="field">
-        <p class="control has-icons-left">
-          <input class="input" type="password" name="password" placeholder="Password" required>
-          <span class="icon is-small is-left">
-            <i class="fa-solid fa-lock"></i>
-          </span>
-        </p>
-      </div>
-
-      <div class="field mt-5">
-        <p class="control">
-          <input type="submit" name="submit" value="Aggiorna password" class="button is-link is-fullwidth is-medium">
-        </p>
-      </div>
-
-    </form>
-
-  </div>
-    
-  <?php require("../footer.php"); ?>
-
-</body>
-</html>
+?>
