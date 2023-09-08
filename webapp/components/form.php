b463d6a47c69b9135078f49158f72b0ecf6e8742
Luca Favini <luca.favini.02@gmail.com>
2023-09-08 16:30:53 +0200

    web(components): add `form`

---
+++ webapp/components/form.php
@@ -0,0 +1,109 @@
+<?php
+
+/* VARIABLES REQUIRED BY THIS COMPONENT:
+
+$error = "";
+
+$CUR_PAGE = "";
+$fa_icon = "";
+$title = "";
+$subtitle = "";
+
+$class = "";
+$help = "";
+
+$inputs = array(
+  array(
+    type=>"hidden",
+    name=>"",
+    value=>""
+  ).
+  array(
+    type=>"select",
+    ...
+  )
+  array(
+    type=>"text",
+    label=>"",
+    name=>"",
+    value=>"",
+    placeholder=>"",
+    required=>"",
+    readonly=>"",
+    icon=>"",
+    help=>""
+  )
+)
+
+*/
+
+// authorization
+require("../scripts/redirector.php");
+  
+// head and navbar
+require("../head.php");
+require("../navbar.php");
+
+?>
+
+<div class="container is-max-desktop">
+
+  <?php if (isset($error)): ?>
+    <div class="notification is-danger is-light mt-6">
+      <strong>Errore: </strong><?= $error ?>.
+    </div>
+  <?php endif ?>
+
+  <form class="box p-6" action="" method="post">
+
+    <span class="icon-text">
+      <span class="icon is-large">
+        <i class="fa-solid <?= $fa_icon ?> fa-2xl"></i>
+      </span>
+      <h1 class="title mt-2"><?= $title ?></h1>
+    </span>
+    <h2 class="subtitle"><?= $subtitle ?></h2>
+
+    <?php foreach ($inputs as $input): ?>
+      
+      <?php if ($input["type"] == "hidden"): ?>
+        <input type="hidden" name="<?= $input["name"] ?>" value="<?= $input["value"] ?>" required readonly>
+
+      <?php elseif ($input["type"] == "select"): ?>
+        <!-- select -->
+
+      <?php else: ?>
+        <div class="field">
+          <label class="label mt-5"><?= $input["label"] ?></label>
+          <p class="control has-icons-left">
+            <input
+              class="input"
+              type="<?= $input["type"] ?>"
+              name="<?= $input["name"] ?>"
+              value="<?= $input["value"] ?>"
+              placeholder="<?= $input["placeholder"] ?>"
+              <?= $input["required"] ?>
+              <?= $input["readonly"] ?>
+            >
+            <span class="icon is-small is-left">
+              <i class="fa-solid <?= $input["icon"] ?>"></i>
+            </span>
+          </p>
+          <p class="help"><?= $input["help"] ?></p>
+        </div>
+      <?php endif ?>
+
+    <?php endforeach ?>
+
+    <div class="field mt-5">
+      <p class="control">
+        <input type="submit" name="submit" value="<?= $title ?>" class="button <?= $class ?> is-fullwidth is-medium">
+      </p>
+      <p class="help <?= $class ?>"><?= $help ?></p>
+    </div>
+  
+  </form>
+
+</div>
+  
+<?php require("../footer.php"); ?>
