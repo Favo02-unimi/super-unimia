9c9850ce2f695272fa88f44936020f42a3012518
Luca Favini <luca.favini.02@gmail.com>
2023-09-10 00:58:11 +0200

    web(*): convert `index` to use `head` component

--- webapp/index.php
+++ webapp/index.php
@@ -1,22 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-  <link rel="stylesheet" href="styles/index.css">
-  <script src="https://kit.fontawesome.com/eb793f993c.js" crossorigin="anonymous"></script>
-  <title>Login - SuperUnimia</title>
-</head>
-<body class="has-background-dark has-text-light">
+<?php
 
-  <?php
+$title = "Login";
+require("../components/head.php");
 
-  require_once("scripts/utils.php");
+require_once("scripts/utils.php");
 
-  require("scripts/index_redirector.php");
+require("scripts/index_redirector.php");
 
-  ?>
+?>
 
   <div class="container is-max-desktop">
 
@@ -76,7 +67,4 @@
     
   </div>
 
-  <?php require("components/footer.php"); ?>
-
-</body>
-</html>
+<?php require("components/footer.php"); ?>
